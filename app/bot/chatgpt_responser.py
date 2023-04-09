9f309da7a1a20d9c63496da37c35354c6df718e7
FrayeZer <69118666+FrayeZer@users.noreply.github.com>
2023-04-09 17:38:39 +0300

    Update chatgpt_responser.py

--- app/bot/chatgpt_responser.py
+++ app/bot/chatgpt_responser.py
@@ -4,7 +4,7 @@ import json_reader
 openai.api_key = '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'
 
 
-def chatgpt_response(prompt: str) -> str:
+def chatgpt_response(interaction, prompt: str) -> str:
     """
     Делает запросы к API chatgpt с использованием модели gpt-3.5-turbo. Возвращает текстовый ответ.
     """
@@ -12,6 +12,6 @@ def chatgpt_response(prompt: str) -> str:
         model='gpt-3.5-turbo',
         messages=[{'role': 'system', 'content': prompt}],
         temperature=0.6,
-        max_tokens=json_reader.get_value('config.json', 'max_tokens'),
+        max_tokens=json_reader.get_value(f'{interaction.guild.id}/config.json', 'max_tokens'),
     )
     return response['choices'][0]['message']['content'].strip()
